fc0864d8e02ecc9326ef8bec2cf44be50c5b2180
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2018-05-17 08:51:55 +0200

    msm8996: enable booting with large kernel image
    Update the base addresses for ramdisk to allow larger kernel image to boot
    properly. otherwise, uncompressing the kernel might end up overwritting the
    ramdisk and/or dtb.
    
    Change-Id: Iafa966b7636e342efb7a642df57db29163ab79a2
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- platform/msm8996/include/platform/iomap.h
+++ platform/msm8996/include/platform/iomap.h
@@ -691,8 +691,8 @@ unsigned int usb_phy_bcr();
 
 #define DDR_START                    platform_get_ddr_start()
 #define ABOOT_FORCE_KERNEL_ADDR      (DDR_START + 0x8000)
-#define ABOOT_FORCE_RAMDISK_ADDR     (DDR_START + 0x2200000)
-#define ABOOT_FORCE_TAGS_ADDR        (DDR_START + 0x2000000)
+#define ABOOT_FORCE_RAMDISK_ADDR     (DDR_START + 0x4000000)
+#define ABOOT_FORCE_TAGS_ADDR        (DDR_START + 0x3E00000)
 #define ABOOT_FORCE_KERNEL64_ADDR    (DDR_START + 0x80000)
 
 #endif
